f22349a43c51d0d1b0c92e906ce01a5111434998
santiarcila23 <santiarcila270@gmail.com>
2026-06-09 21:33:43 -0500

    Fix start.sh sin set -e

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e
 cd /var/www/html
-/usr/local/bin/php artisan config:clear
-/usr/local/bin/php artisan migrate --force
+/usr/local/bin/php artisan config:clear || true
+/usr/local/bin/php artisan migrate --force || true
 echo "Starting server on port ${PORT:-8000}"
 exec /usr/local/bin/php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
