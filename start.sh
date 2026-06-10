6088b0af4139a69b05e6045235f2e4354204c070
santiarcila23 <santiarcila270@gmail.com>
2026-06-09 20:35:01 -0500

    Fix ruta php en start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-php artisan config:clear
-php artisan migrate --force
-php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+/usr/local/bin/php artisan config:clear
+/usr/local/bin/php artisan migrate --force
+/usr/local/bin/php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
