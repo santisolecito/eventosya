5493ef217437f9c8fb8f6747b84a35382bb3eeba
santiarcila23 <santiarcila270@gmail.com>
2026-06-09 20:43:13 -0500

    Fix exec en start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+cd /var/www/html
 /usr/local/bin/php artisan config:clear
 /usr/local/bin/php artisan migrate --force
-/usr/local/bin/php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
+echo "Starting server on port ${PORT:-8000}"
+exec /usr/local/bin/php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
