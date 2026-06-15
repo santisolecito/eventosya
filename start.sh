59af3a86dbeaf7b1e34c59676c5778126438c18d
santiago alzate <santisolecito@gmail.com>
2026-06-15 16:52:57 -0500

    img bug on events solved

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ set -e
 cd /var/www/html
 /usr/local/bin/php artisan config:clear
 /usr/local/bin/php artisan migrate --force
+/usr/local/bin/php artisan storage:link --force
 echo "Starting server on port ${PORT:-8000}"
 exec /usr/local/bin/php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
